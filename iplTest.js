533ca01ee15477dec0034d154b62fa3aa341d0eb
Rajesh <rjesh13004034@gmail.com>
2018-11-22 21:36:19 +0530

    Ploted the number of matches played per year of all the years in IPL

--- iplTest.js
+++ iplTest.js
@@ -1,4 +1,14 @@
 var r=require('xlsx');
 var wb=r.readFile('./matches.xlsx');
 let ar=r.utils.sheet_to_json(wb.Sheets.Sheet1);
-console.log(ar);
+//console.log(ar);
+let yearlyMatches={};
+let matchesWonYearly={}
+let yearlyId={};
+let id=0;
+for(let i in ar){
+	if(yearlyMatches[ar[i].season]){yearlyMatches[ar[i].season]++;}
+	else{yearlyMatches[ar[i].season]=1;matchesWonYearly[ar[i].season]=[];}
+}
+for(let i in yearlyMatches)
+console.log(i+":"+yearlyMatches[i]);
